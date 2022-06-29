29ce2630f5fbe8e216eba98163298a97bfc74c46
olkone <konecnyoliver@gmail.com>
2022-06-29 16:48:25 -0500

    Creates mouseover effect function

--- sketch.js
+++ sketch.js
@@ -2,12 +2,22 @@ function addSqaures(number) {
 
     const grid = document.querySelector('#grid');
     
-
-    for (let i = 0; i < (number * number); i++) {
+    // number arguement represents the length of one side, therefore is squared
+    for (let i = 0; i < (number ** 2); i++) {
         const square = document.createElement('div');
         grid.appendChild(square);
-        square.classList.add("square");
+        square.classList.add('square');
     }
 }
 
 addSqaures(16);
+
+function hoverEffect(e) {
+    e.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'green'
+    })
+}
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => hoverEffect(square));
