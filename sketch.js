96aee868d26dc95d630e2af33e2b501200137b2b
olkone <konecnyoliver@gmail.com>
2022-06-29 21:31:09 -0500

    Adds dynamic grid adjustment

--- sketch.js
+++ sketch.js
@@ -8,14 +8,25 @@ function addSqaures(number) {
         grid.appendChild(square);
         square.classList.add('square');
     }
+
+    const squares = document.querySelectorAll('.square');
+    const gridWidth = document.getElementById('grid').offsetWidth;
+
+    squares.forEach((square) => {
+        square.style.height = `${(gridWidth/number)}px`;
+        square.style.width = `${(gridWidth/number)}px`;
+    });
 }
 
 addSqaures(16);
 
 function hoverEffect(e) {
     e.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'green'
-    })
+        e.target.style.backgroundColor = 'green';
+        setTimeout( () => {
+            e.target.style.backgroundColor = "";
+        }, 500);
+    }, false);
 }
 
 const squares = document.querySelectorAll('.square');
