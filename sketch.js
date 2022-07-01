bf3cbc9d02832fe28db1ed231d21df48f8ca17f4
olkone <konecnyoliver@gmail.com>
2022-06-30 21:44:50 -0500

    Started js from scratch; listens for button toggle for color value

--- sketch.js
+++ sketch.js
@@ -1,96 +1,50 @@
-const editBtn = document.querySelector('#edit-button');
-const clearBtn = document.querySelector('#clear-button');
-const colorBtns = document.querySelectorAll('.color-buttons');
-const slider = document.querySelector('#grid-slider');
 
-function hoverEffect(e, color='black') {
-    e.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = `${color}`;
-    });
-}
+const buttons = document.querySelectorAll('.color-buttons');
+const active = document.querySelectorAll('.active');
 
-colorBtns.forEach(button => {
+buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        const squares = document.querySelectorAll('.square');
-        const btnId = button.getAttribute('id');
-        const random = "#" + ((1<<24)*Math.random() | 0).toString(16);
-
-        switch(btnId) {
-            default:
-                choice = 'black';
-                break;
-                
-            case 'black':
-            case 'blue':
-                choice = btnId;
-                break;
-
-            case 'random-color':
-                choice = random;
-                break;
-            
-            case 'eraser':
-                choice = 'transparent';
-                break;
-        }
-        squares.forEach(square => hoverEffect(square, choice));
+        buttons.forEach((e) => {
+            e.classList.remove('active')
+        })
+        button.classList.toggle('active');
     });
 });
 
-function addSqaures(number=16) {
-    // Removes any pre-existing squares when editing the grid
-    document.querySelectorAll('.square').forEach(square => square.remove());
 
+function toggleColor() {
+    const activeButton = document.querySelector('.active');
+    const newColor = activeButton.getAttribute('id');
+
+    if (newColor === 'party') {
+        return ('#' + ((1<<24)*Math.random() | 0).toString(16));
+    } else {
+        return newColor;
+    }
+    
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = toggleColor();
+}
+
+function makeGrid(number=16) {
     const grid = document.querySelector('#grid');
 
-    for (let i = 0; i < (number ** 2); i++) {
+    for (let i = 0; i < (number**2); i++) {
         const square = document.createElement('div');
         grid.appendChild(square);
         square.classList.add('square');
     }
 
-    let squares = document.querySelectorAll('.square');
-    const gridWidth = document.querySelector('#grid').offsetWidth;
+    const squares = document.querySelectorAll('.square');
+    const gridWidth = grid.offsetWidth;
 
     squares.forEach((square) => {
         square.style.height = `${(gridWidth/number)}px`;
         square.style.width = `${(gridWidth/number)}px`;
+        square.addEventListener('mouseover', changeColor);
     });
-
-    squares.forEach(square => hoverEffect(square));
 }
 
-function editGrid() {
-    editBtn.addEventListener('click', () => {
-        // Validate user input
-        let input;
-        while (input < 1 || input > 100 || Number.isInteger(input) === false) {
-            input = Number(prompt("Choose a number between 1 and 100"));
-        }
-        addSqaures(input);
-    });
-    // Re-apply hover effect to new grid
-    let squares = document.querySelectorAll('.square');
-    squares.forEach(square => hoverEffect(square));
-}
-
-function sliderGrid() {
-    const input = slider.value;
-    addSqaures(input)
-
-    let squares = document.querySelectorAll('.square');
-    squares.forEach(square => hoverEffect(square));
-}
-
-function clearGrid() {
-    document.querySelectorAll('.square').forEach((square) => {
-        square.style.backgroundColor = 'transparent';}
-    );
-}
-
-window.onload = addSqaures();
-
-clearBtn.addEventListener('click', clearGrid);
-
-
-slider.addEventListener('change', sliderGrid)
+makeGrid();
